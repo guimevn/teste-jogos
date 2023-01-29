17b2684d357753b37897d29b7b5b4c054c06c320
guimevn <vdnguilherme@gmail.com>
2023-01-29 15:07:45 -0300

    aaaaaaaaaaaaaa

--- My project/Assets/script.cs
+++ My project/Assets/script.cs
@@ -8,7 +8,7 @@ public class script : MonoBehaviour
     void Start()
     {
         // teste
-        Debug.Log("hello, world!");
+        Debug.Log("hello, aaaaaaaaa");
     }
 
     // Update is called once per frame
