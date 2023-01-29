f3a834b39c388acba434e13b15a0b0f43f636c6c
guimevn <vdnguilherme@gmail.com>
2023-01-29 15:06:55 -0300

    alteração script.cs

---
+++ My project/Assets/script.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // teste
+        Debug.Log("hello, world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
